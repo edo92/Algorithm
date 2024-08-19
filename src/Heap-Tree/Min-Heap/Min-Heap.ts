3653323e993c6410b518a25f8a21afa2449fe952
edo92 <edoter92@gmail.com>
2024-08-18 21:27:21 -0700

    setup min heap class

--- src/Heap-Tree/Min-Heap/Min-Heap.ts
+++ src/Heap-Tree/Min-Heap/Min-Heap.ts
@@ -4,4 +4,86 @@ export class MinHeap {
    constructor() {
       this.heap = [];
    }
+
+   public insert(value: number): void {
+      this.heap.push(value);
+      this.heapifyUp(this.heap.length - 1);
+   }
+
+   public extractMin(): number | null {
+      if (this.heap.length === 0) {
+         return null;
+      }
+
+      if (this.heap.length === 1) {
+         return this.heap.pop()!;
+      }
+
+      const root = this.heap[0];
+      this.heap[0] = this.heap.pop()!;
+      this.heapifyDown(0);
+
+      return root;
+   }
+
+   public peek(): number | null {
+      if (this.heap.length === 0) {
+         return null;
+      }
+
+      return this.heap[0];
+   }
+
+   public size(): number {
+      return this.heap.length;
+   }
+
+   public isEmpty(): boolean {
+      return this.heap.length === 0;
+   }
+
+   private getParentIndex(index: number): number {
+      return Math.floor((index - 1) / 2);
+   }
+
+   private getLeftChildIndex(index: number): number {
+      return 2 * index + 1;
+   }
+
+   private getRightChildIndex(index: number): number {
+      return 2 * index + 2;
+   }
+
+   private swap(index1: number, index2: number): void {
+      [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+   }
+
+   private heapifyUp(index: number): void {
+      let parentIndex = this.getParentIndex(index);
+
+      while (index > 0 && this.heap[parentIndex] > this.heap[index]) {
+         this.swap(parentIndex, index);
+         index = parentIndex;
+         parentIndex = this.getParentIndex(index);
+      }
+   }
+
+   private heapifyDown(index: number): void {
+      let smallest = index;
+      const leftChildIndex = this.getLeftChildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
+
+      if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallest]) {
+         smallest = leftChildIndex;
+      }
+
+      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallest]) {
+         smallest = rightChildIndex;
+      }
+
+      if (smallest !== index) {
+         this.swap(index, smallest);
+         this.heapifyDown(smallest);
+      }
+   }
 }
--- src/Heap-Tree/Min-Heap/index.ts
+++ src/Heap-Tree/Min-Heap/index.ts
@@ -1,3 +1,13 @@
 import { MinHeap } from './Min-Heap';
 
-const heap = new MinHeap();
+const minHeap = new MinHeap();
+
+minHeap.insert(10);
+minHeap.insert(15);
+minHeap.insert(20);
+minHeap.insert(17);
+
+console.log(minHeap.extractMin()); // 10
+console.log(minHeap.peek()); // 15
+console.log(minHeap.size()); // 3
+console.log(minHeap.isEmpty());
